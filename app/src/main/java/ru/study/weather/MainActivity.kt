d1b49711ddc704f7b9edbd755a46a9cd632460ca
Crush <art60@rambler.ru>
2020-12-07 10:36:29 +0300

    Update UI

--- app/src/main/java/ru/study/weather/MainActivity.kt
+++ app/src/main/java/ru/study/weather/MainActivity.kt
@@ -2,13 +2,20 @@ package ru.study.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
 import androidx.viewpager.widget.ViewPager
+import com.google.android.material.appbar.CollapsingToolbarLayout
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        findViewById<CollapsingToolbarLayout>(R.id.toolbar_layout).title = "Город + температура"
 
         val viewPager: ViewPager = findViewById(R.id.viewpager)
         viewPager.adapter = AppFragmentPagerAdapter(supportFragmentManager, this)

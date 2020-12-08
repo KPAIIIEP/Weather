b9ae30d4bb7d795e3b3f6151b66ff09db9349814
Crush <art60@rambler.ru>
2020-12-08 16:32:42 +0300

    Fix models, API, UI

--- app/src/main/java/ru/study/weather/WeatherApi.kt
+++ app/src/main/java/ru/study/weather/WeatherApi.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
-import ru.study.weather.model.JsonRoot
+import ru.study.weather.model.WeatherData
 
 private const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
 
@@ -14,7 +14,7 @@ interface WeatherApi {
     fun getForecastDaily(@Query(value = "q", encoded = true) q: String = "Moskva",
                          @Query("units") units: String = "metric",
                          @Query("cnt") cnt: Int = 7,
-                         @Query("appid") appId: String = API_KEY): Call<JsonRoot>
+                         @Query("appid") appId: String = API_KEY): Call<WeatherData>
 
     companion object {
         val weatherApi: WeatherApi = Retrofit.Builder()
---
+++ app/src/main/java/ru/study/weather/model/WeatherDetails.kt
@@ -0,0 +1,20 @@
+package ru.study.weather.model
+
+import com.google.gson.annotations.SerializedName
+
+data class WeatherDetails(
+        @SerializedName("dt")
+        var dateTime: Int,
+        @SerializedName("temp")
+        var temp: Temperature,
+        @SerializedName("pressure")
+        var pressure: Int,
+        @SerializedName("humidity")
+        var humidity: Int,
+        @SerializedName("weather")
+        var weather: List<Weather>,
+        @SerializedName("speed")
+        var speed: Double,
+        @SerializedName("deg")
+        var degrees: Int,
+)
